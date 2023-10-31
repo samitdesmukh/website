24835fe774ba52cb9d5c6212a9e0798176d93272
samit <desmukhkumarsamit@gmail.com>
2023-10-31 22:25:00 +0530

    new changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Add your JavaScript code here
 document.getElementById("whatsappButton").addEventListener("click", function() {
   // Replace the following URL with your own WhatsApp API URL and phone number
-  var whatsappUrl = "https://api.whatsapp.com/send?phone=8335899606&text=Hello%20there!";
+  var whatsappUrl = "https://api.whatsapp.com/send?phone=+91 8335899606&text=Hello%20there!";
   
   // Open WhatsApp in a new tab when the button is clicked
   window.open(whatsappUrl, "_blank");
