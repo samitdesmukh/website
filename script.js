cc8df22618c1f3bc48694ba1afe169a46190133b
samit <desmukhkumarsamit@gmail.com>
2023-11-19 15:46:43 +0530

    first changes

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-// Add your JavaScript code here
-document.getElementById("whatsappButton").addEventListener("click", function() {
-  // Replace the following URL with your own WhatsApp API URL and phone number
-  var whatsappUrl = "https://api.whatsapp.com/send?phone=+91 8335899606&text=Hello%20there!";
+// // Add your JavaScript code here
+// document.getElementById("whatsappButton").addEventListener("click", function() {
+//   // Replace the following URL with your own WhatsApp API URL and phone number
+//   var whatsappUrl = "https://api.whatsapp.com/send?phone=+91 8335899606&text=Hello%20there!";
   
-  // Open WhatsApp in a new tab when the button is clicked
-  window.open(whatsappUrl, "_blank");
-});
+//   // Open WhatsApp in a new tab when the button is clicked
+//   window.open(whatsappUrl, "_blank");
+// });
+
+// //https://api.whatsapp.com/send?phone=XXXXXXXXXXX
+
+// 20rinkisamanta01@gmail.com
